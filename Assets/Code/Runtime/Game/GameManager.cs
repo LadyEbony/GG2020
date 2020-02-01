9c3b4a4ea4da1771b632c22f5b15824aba9b3b1b
theclone <mrperson7895@gmail.com>
2020-02-01 15:52:15 -0800

    Add GameManager/Team Selection Canvas

---
+++ Assets/Code/Runtime/Game/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject TeamSelectionPanel;
+    List<GameObject> builders;
+    GameObject destroyer;
+
+    public void AddDestroyerPlayer()
+    {
+        Debug.Log("Joined kaiju team");
+    }
+
+    public void AddBuilderPlayer()
+    {
+        Debug.Log("Joined repair team");
+    }
+}
