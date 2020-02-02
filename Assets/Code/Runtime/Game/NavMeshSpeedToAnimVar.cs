1b8195221637be85e1b725b3a9f040359bb72f20
Kevin Huang <khuan028@ucr.edu>
2020-02-02 14:42:19 -0800

    Omar, here are your prefabs for Fixer and Kaiju

---
+++ Assets/Code/Runtime/Game/NavMeshSpeedToAnimVar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshSpeedToAnimVar : MonoBehaviour
+{
+  public NavMeshAgent navMeshAgent;
+  public Animator animator;
+  public string animatorVariable = "WalkSpeed";
+
+  private void Update()
+  {
+    if(navMeshAgent && animator)
+    {
+      animator.SetFloat(animatorVariable, navMeshAgent.velocity.magnitude / navMeshAgent.speed);
+    }
+  }
+}
