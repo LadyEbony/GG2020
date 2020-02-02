c04e5df63b4cd0bc2a5703a75672cb2f274f788c
LadyEbony <jgarc129@ucr.edu>
2020-02-02 12:39:40 -0800

    Added character select

---
+++ Assets/Code/Runtime/Game/Character Select/TeamManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamManager : MonoBehaviour {
+
+  public static TeamManager Instance { get; private set; }
+
+  private void Awake() {
+    Instance = this;
+  }
+
+  [System.Serializable]
+  public class Character{
+    public string name;
+    public Color outlineColor, innerColor;
+    public Sprite fullSprite, headSprite;
+  }
+
+  public Character[] Characters;
+  public Character GetCharacter(int index) => Characters[index];
+
+}
